9eae71a8bf1d6827195716ea9db611ec03ddd966
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-17 01:09:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
 def square_array(array)
+  counter = 0
+  new_array[]
   array.each do |number|
     new_number = number*number
-    
+    new_array[counter] = new_number
+    counter ++
   end
+  return new_array
 end
